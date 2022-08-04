14dd5d2260720ce37f9460d24fb51fcd6b6b909b
Anastasia Tonkikh <ana.tonkikh@gmail.com>
2022-08-04 20:50:32 +0300

    adding new class MyTest

--- src/test/java/guru/qa/MyTest.java
+++ src/test/java/guru/qa/MyTest.java
@@ -1,2 +1,21 @@
-package guru.qa;public class MyTest {
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+    public static void main(String[] args) {
+        int day = 1;
+        switch (day) {
+            case 1:
+                System.out.println("Today is Monday");
+                break;
+            case 2:
+                System.out.println("Today is Tuesday");
+                break;
+            case 3:
+                System.out.println("Today is Wednesday");
+                break;
+        }
+    }
 }
